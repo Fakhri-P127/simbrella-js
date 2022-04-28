5510b80e9c738a2c41a3e2c2b19c126e208fd886
Fakhri-P127 <fakhrita@code.edu.az>
2022-04-28 12:56:58 +0400

    added transition

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,12 @@ const btnBar = document.querySelector("#btnBar");
 const hiddenDiv = document.querySelector("#hiddenDiv");
 const btnClose = document.querySelector("#btnClose");
 
-console.log(hiddenDiv);
-// const overlay = document.querySelector("#overlay--clas");
 btnBar.addEventListener("click", function () {
   document.querySelector("nav").classList.add("btnAll");
   hiddenDiv.classList.remove("hidden--div");
-  hiddenDiv.classList.add("flexs");
+  hiddenDiv.classList.add("make-visible");
+  btnBar.classList.add("rotate--icons");
+  document.querySelector("nav").style.transition = "all 350ms ease-in-out";
   btnBar.classList.add("make-hidden");
   btnClose.classList.remove("make-hidden");
 });
@@ -15,7 +15,9 @@ btnBar.addEventListener("click", function () {
 btnClose.addEventListener("click", function () {
   document.querySelector("nav").classList.remove("btnAll");
   hiddenDiv.classList.add("hidden--div");
-  hiddenDiv.classList.remove("flexs");
+  hiddenDiv.classList.remove("make-visible");
   btnBar.classList.remove("make-hidden");
   btnClose.classList.add("make-hidden");
+  btnClose.classList.add("rotate--icons");
+  document.querySelector("nav").style.transition = "all 0.5s ease-in-out";
 });
